220f3ab398430cddf8ec42d81c07ee82dd2ca444
aishukr18 <aishukrgowda2003@gmail.com>
2025-08-27 11:48:31 +0530

    Create router_dst_sequencer.sv

---
+++ ROUTER1x3/dst_agt_top/router_dst_sequencer.sv
@@ -0,0 +1,11 @@
+class router_dst_sequencer extends uvm_sequencer #(dst_xtn);
+
+	`uvm_component_utils(router_dst_sequencer)
+
+	extern function new(string name = "router_dst_sequencer",uvm_component parent);
+endclass
+
+function router_dst_sequencer::new(string name="router_dst_sequencer",uvm_component parent);
+	super.new(name,parent);
+endfunction
+
