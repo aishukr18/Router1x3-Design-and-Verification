9f4341bee3000edd93759468ae00f626a5c98c61
aishukr18 <aishukrgowda2003@gmail.com>
2025-08-27 11:27:06 +0530

    Create router_src_sequencer.sv

---
+++ ROUTER1x3/src_agt_top/router_src_sequencer.sv
@@ -0,0 +1,9 @@
+class router_src_sequencer extends uvm_sequencer#(src_xtn);
+	`uvm_component_utils(router_src_sequencer)
+	
+	extern function new(string name = "router_src_sequencer",uvm_component parent);
+endclass
+
+function router_src_sequencer::new(string name = "router_src_sequencer",uvm_component parent);
+	super.new(name,parent);
+endfunction
