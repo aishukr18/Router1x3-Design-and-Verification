0b2e00a9fc499d38ada7393b8c1474d74d6a7f61
aishukr18 <aishukrgowda2003@gmail.com>
2025-08-27 11:33:01 +0530

    Create router_src_agent_config.sv

---
+++ ROUTER1x3/src_agt_top/router_src_agent_config.sv
@@ -0,0 +1,14 @@
+class router_src_agent_config extends uvm_object;
+
+	`uvm_object_utils(router_src_agent_config)
+	
+	uvm_active_passive_enum is_active = UVM_ACTIVE;
+
+	virtual router_if vif;
+	
+	extern function new(string name = "router_src_agent_config");
+endclass
+
+function router_src_agent_config::new(string name = "router_src_agent_config");
+	super.new(name);
+endfunction
